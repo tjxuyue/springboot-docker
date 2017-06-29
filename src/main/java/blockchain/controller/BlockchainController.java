38eb41f273670286d4b0134b6786f2708c0b19d5
tjxuyue <tjxuyue@163.com>
2017-06-29 23:57:30 +0800

    init project

---
+++ src/main/java/blockchain/controller/BlockchainController.java
@@ -0,0 +1,15 @@
+package controller
+
+
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BlockchainController {
+	@RequestMapping("/test")
+	public String test(){
+		return "test";
+	}
+}
